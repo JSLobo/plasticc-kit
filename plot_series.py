b1da10cc9b13c611e07dcfe3688f8b2f350b45c7
cdanmontoya <carlos.danmontoya@gmail.com>
2019-03-04 16:49:16 -0500

    new changes

---
+++ plot_series.py
@@ -0,0 +1,8 @@
+from bokeh.plotting import figure, output_file, show
+from bokeh.palettes import Set1
+from bokeh.io import output_notebook, show
+from bokeh.models import HoverTool
+from collections import OrderedDict
+
+class Plot_series():
+    
